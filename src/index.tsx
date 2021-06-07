4ab3250ab062bd454f1a7381d3db82594281d36c
Ivan Bilokon <Belokonivan1707@gmail.com>
2021-06-07 16:06:37 +0300

    add base structure

---
+++ src/index.tsx
@@ -0,0 +1,14 @@
+// import ReactDOM from "react";
+// import React from 'react'
+import * as React from 'react';
+import * as ReactDOM from 'react-dom';
+
+const App = () => {
+
+
+  
+  return <div>allow<h1>
+    </h1></div>
+}
+
+ReactDOM.render( <App/>, document.getElementById('root') as HTMLElement )
